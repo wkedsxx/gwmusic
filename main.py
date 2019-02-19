f1a0faab757a19c22587f988258e33677d6885ed
wkedsxx <dawid.debest@gmail.com>
2019-02-19 14:50:42 +0100

    Added First row in Box Layout

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from gi.repository.GdkPixbuf import Pixbuf
 
+class ListBoxRowAlbumCover(Gtk.ListBoxRow):
+    def __init__(self, coverImage):
+        super(Gtk.ListBoxRow, self).__init__()
+        self.coverImage = coverImage
+
 
 class MyWindow(Gtk.Window):
 
@@ -11,10 +16,17 @@ class MyWindow(Gtk.Window):
         self.set_default_size(320, 240)
         self.fullscreen()
 
-        self.button = Gtk.Button(label="X")
-        self.button.connect("clicked", self.on_X_clicked)
-        self.add(self.button)
+        self.box1 = Gtk.Box()
+        self.add(self.box1)
 
+
+        self.button1 = Gtk.Button(label="X")
+        
+        self.button1.connect("clicked", self.on_X_clicked)
+        self.box1.set_size_request(10, 0)
+        self.box1.pack_start(self.button1, False, False, 0)
+        
+        
     def on_X_clicked(self, widget):
         Gtk.main_quit()
     
